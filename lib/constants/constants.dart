93dad0644cb6a23a77b176bd156e8d80f7b1baa5
dagmayalew <dagmayalew489@gmail.com>
2024-02-29 10:59:49 +0300

    Initial commit: Create the home page with basic layout and design.

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+const double kdouble5 = 5.0;
+const double kdouble10 = 10.0;
+const double kdouble15 = 15.0;
+const double kdouble20 = 20.0;
+const double kdouble25 = 25.0;
+const double kdouble30 = 30.0;
+const double kdouble40 = 40.0;
